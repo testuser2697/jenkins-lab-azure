532659ab3faf2337eeb5f0ab986162e2f030f323
testuser2697 <TestUser2697@outlook.com>
2024-12-13 00:49:03 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.18.0"
+    }
+  }
+
+  backend "azurerm" {
+    storage_account_name = "<your-storage-account-name>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "example-vnet"
+  location            = "East US"
+  resource_group_name = "RG1"
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "lab"
+  }
+}
