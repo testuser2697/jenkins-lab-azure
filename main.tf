2392108242d64de7700006505fe47dec0bace07c
testuser2697 <TestUser2697@outlook.com>
2024-12-27 12:12:57 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.18.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG1"
+    storage_account_name = "<your storage account>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "RG2"
+  location = "westeurope"
+}
