e86bc29b43769d39d3185ea4f2460d1a5bfbe92b
testuser2697 <TestUser2697@outlook.com>
2025-07-10 12:58:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatemichael"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
