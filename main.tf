04f2885834f2680e5bdb1ddec922033ff1740e5a
testuser2697 <TestUser2697@outlook.com>
2025-07-10 12:55:09 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "04e1755a-f772-4cf0-a1bc-18c1f6f8570e"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "RG1"
+    storage_account_name = "<your storage account>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "RG2"
+  location = "westeurope"
+}
